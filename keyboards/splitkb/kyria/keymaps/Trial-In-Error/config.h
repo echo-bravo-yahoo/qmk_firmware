74bb6add8f5b9083265402b04e51eb8051e93eb6
Ashton Eby <ashton.eby@oracle.com>
2022-03-15 19:40:19 -0700

    WIP

--- keyboards/splitkb/kyria/keymaps/Trial-In-Error/config.h
+++ keyboards/splitkb/kyria/keymaps/Trial-In-Error/config.h
@@ -16,7 +16,7 @@
 
 #pragma once
 
-#define USER_PRINT
+// #define USER_PRINT
 
 #ifdef RGBLIGHT_ENABLE
 #    define RGBLIGHT_ANIMATIONS
@@ -30,13 +30,17 @@
 #define IGNORE_MOD_TAP_INTERRUPT
 
 // Debug for userprint messages only
-#define USER_PRINT
+// #define USER_PRINT
 
 // transfer / sync
 #define SPLIT_TRANSACTION_IDS_USER USER_SYNC_A, USER_SYNC_B, USER_SYNC_C
-// Primary to secondary:
-#define RPC_M2S_BUFFER_SIZE 48
-// Secondary to primary:
+// Master to slave:
+#define RPC_M2S_BUFFER_SIZE (21 * 4 + 1)
+// Slave to master:
 #define RPC_S2M_BUFFER_SIZE 48
 
 #include <stdbool.h>
+
+#define SERIAL_SCREEN_BUFFER_LENGTH (/*SSD1306_MatrixCols*/21 * /*SSD1306_MatrixRows*/4 + /*Extra IsEnabledBit*/1)
+// extern volatile bool hid_screen_change; // Flag marking if the screen data is dirty and needs transferring to slave
+// extern volatile uint8_t serial_slave_screen_buffer[SERIAL_SCREEN_BUFFER_LENGTH];
